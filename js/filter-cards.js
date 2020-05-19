222e0c4bd0042a8ce584a902621c97f25dda8399
Marcella <marcellamaki@gmail.com>
2020-05-19 18:01:15 -0400

    update card functionality and add modal

--- js/filter-cards.js
+++ js/filter-cards.js
@@ -1,40 +1,39 @@
 // Update display to seach and filter products
 var $ = require('jquery');
 
-$('#product-search-form').submit(function (e) {
-  e.preventDefault();
-  var filter = $( '#search-field').val();
-  var products = document.getElementsByName('productCard');
-  for (i = 0; i < products.length; i++) {
-    if (products[ i ].getElementsByTagName('h4')[ 0 ]) {
-      productName = products[ i ].getElementsByTagName('h4')[ 0 ].innerText.toLowerCase();
-      productNameSlugified = productName.replace(/\s/g, '-');
-      if (productName.includes(filter)) {
-        $('#product-card-' + productNameSlugified).removeClass('pc-inactive');
-        console.log(productName);
-      } else {
-        $('#product-card-' + productNameSlugified).addClass('pc-inactive');
-        console.log("no match", productName);
-      }
-    }
-  }
+console.log("connected!")
+//
+// $('#product-search-form').submit(function (e) {
+//   e.preventDefault();
+//   var filter = $( '#search-field').val();
+//   var products = document.getElementsByName('productCard');
+//   for (i = 0; i < products.length; i++) {
+//     if (products[ i ].getElementsByTagName('h4')[ 0 ]) {
+//       productName = products[ i ].getElementsByTagName('h4')[ 0 ].innerText.toLowerCase();
+//       productNameSlugified = productName.replace(/\s/g, '-');
+//       if (productName.includes(filter)) {
+//         $('#product-card-' + productNameSlugified).removeClass('pc-inactive');
+//         console.log(productName);
+//       } else {
+//         $('#product-card-' + productNameSlugified).addClass('pc-inactive');
+//         console.log("no match", productName);
+//       }
+//     }
+//   }
+// });
+
+$('.usa-card').on('click', function () {
+  $( this ).find('.modal').removeClass('modal-inactive').addClass('modal-active');
 });
 
-$('#product-filter-form').submit(function (e) {
-  e.preventDefault();
-  var filter = $( '#search-field').val();
-  var products = document.getElementsByName('productCard');
-  for (i = 0; i < products.length; i++) {
-    if (products[ i ].getElementsByTagName('h4')[ 0 ]) {
-      productName = products[ i ].getElementsByTagName('h4')[ 0 ].innerText.toLowerCase();
-      productNameSlugified = productName.replace(/\s/g, '-');
-      if (productName.includes(filter)) {
-        $('#product-card-' + productNameSlugified).removeClass('pc-inactive');
-        console.log(productName);
-      } else {
-        $('#product-card-' + productNameSlugified).addClass('pc-inactive');
-        console.log("no match", productName);
-      }
-    }
-  }
+
+
+$(".close").on("click", function () {
+  $(".modal-active").removeClass('modal-active').addClass('modal-inactive');
 });
+
+$(window).onclick = function(e) {
+  if (e.target !== $("#modal")) {
+    $(".modal-active").removeClass('modal-active').addClass('modal-inactive');
+  }
+}

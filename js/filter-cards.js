3a5a3851e3078108ceab852bd6ee6e018adc6f3a
Marcella <marcellamaki@gmail.com>
2020-05-20 09:28:55 -0400

    add filter by agency. styling still WIP

--- js/filter-cards.js
+++ js/filter-cards.js
@@ -30,11 +30,12 @@ $('#product-filter-form').submit(function (e) {
     for (j = 0; j < products.length; j++ ) {
       productYear = products[ j ].getElementsByTagName('h3')[ 0 ].innerText;
       productTopic = products[ j ].getElementsByTagName('h4')[ 0 ].innerText;
+      productAgency = products[ j ].getElementsByTagName('h5')[ 0 ].innerText;
       productName = products[ j ].getElementsByTagName('h2')[ 0 ].innerText.toLowerCase();
       productNameSlugified = slugify(productName.split('.').join("-").split(':').join("-"));
       if (selectedValues[ i ].checked == true ) {
         var filter = selectedValues[ i ].value
-        if (filter == productTopic || filter == productYear) {
+        if (filter == productTopic || filter == productYear || productAgency.includes(filter)) {
           $('#product-card-' + productNameSlugified).removeClass('pc-inactive');
         } else {
           $('#product-card-' + productNameSlugified).addClass('pc-inactive');

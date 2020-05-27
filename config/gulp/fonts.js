87b63a857137cdd26997c9a5bebe3d05e170c9c0
Marcella <marcellamaki@gmail.com>
2020-05-27 13:36:29 -0400

    update fonts

--- config/gulp/fonts.js
+++ config/gulp/fonts.js
@@ -2,7 +2,7 @@ var gulp  = require('gulp');
 var dutil = require('./doc-util');
 var task  = 'fonts';
 
-gulp.task(task, function (done) {
+gulp.task('copy-uswds-fonts', function (done) {
 
   dutil.logMessage(task, 'Copying fonts from uswds');
 
@@ -12,3 +12,28 @@ gulp.task(task, function (done) {
   return stream;
 
 });
+
+
+gulp.task('copy-custom-fonts', function (done) {
+
+  dutil.logMessage(task, 'Copying custom fonts');
+
+  var stream = gulp.src('./fonts/**/*')
+    .pipe(gulp.dest('assets/fonts'));
+
+  return stream;
+
+});
+
+gulp.task(task,
+  gulp.series(
+    function (done) {
+      dutil.logMessage(task, 'Copying fonts');
+      done();
+    },
+    gulp.parallel(
+      'copy-uswds-fonts',
+      'copy-custom-fonts'
+    )
+  )
+);

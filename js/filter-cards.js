7cffb32b83f2eb34abc149287215e8fd02c80610
Marcella <marcellamaki@gmail.com>
2020-05-28 18:57:00 -0400

    filter cards by keywords

--- js/filter-cards.js
+++ js/filter-cards.js
@@ -76,3 +76,26 @@ $('.data-card-group').on('click', function (e) {
       }
     }
   });
+
+
+
+  $('#data-search-form').submit(function (e) {
+    e.preventDefault();
+    var filter = $( '#search-field').val();
+    var dataSets = document.getElementsByName('data-set-card');
+    for (i = 0; i < dataSets.length; i++) {
+      if (dataSets[ i ].getElementsByTagName('h2')[ 0 ]) {
+        dataName = dataSets[ i ].getElementsByTagName('h2')[ 0 ].innerText
+        dataPS = dataSets[ i ].getElementsByTagName('h3')[ 0 ].innerText
+        dataDescription = dataSets[ i ].getElementsByTagName('p')[ 0 ].innerText
+        dataNameSlugified = slugify(dataName.toLowerCase().replace('(', ' ').replace(')', '').replace('–⁠', ' ').replace('-', ' ').replace('&', '').replace('*', ' ').split(" ").join("-"));
+        if (dataName.toLowerCase().includes(filter) || dataPS.toLowerCase().includes(filter) || dataDescription.toLowerCase().includes(filter)) {
+          console.log(dataNameSlugified);
+          $('#data-set-card-' + dataNameSlugified).removeClass('pc-inactive');
+        } else {
+          console.log("mismatch", dataNameSlugified);
+          $('#data-set-card-' + dataNameSlugified).addClass('pc-inactive');
+        }
+      }
+    }
+  });

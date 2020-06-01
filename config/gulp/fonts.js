3da75eb3cd94d74f011738f801d3810747a248ba
Marcella <marcellamaki@gmail.com>
2020-06-01 11:08:08 -0400

    reset gulp file

--- config/gulp/fonts.js
+++ config/gulp/fonts.js
@@ -2,8 +2,7 @@ var gulp  = require('gulp');
 var dutil = require('./doc-util');
 var task  = 'fonts';
 
-gulp.task('copy-uswds-fonts', function (done) {
-
+gulp.task(task, function (done) {
   dutil.logMessage(task, 'Copying fonts from uswds');
 
   var stream = gulp.src('./node_modules/uswds/src/fonts/**/*')
@@ -14,26 +13,26 @@ gulp.task('copy-uswds-fonts', function (done) {
 });
 
 
-gulp.task('copy-custom-fonts', function (done) {
-
-  dutil.logMessage(task, 'Copying custom fonts');
-
-  var stream = gulp.src('./fonts/**/*')
-    .pipe(gulp.dest('assets/fonts'));
-
-  return stream;
-
-});
-
-gulp.task(task,
-  gulp.series(
-    function (done) {
-      dutil.logMessage(task, 'Copying fonts');
-      done();
-    },
-    gulp.parallel(
-      'copy-uswds-fonts',
-      'copy-custom-fonts'
-    )
-  )
-);
+// gulp.task('copy-custom-fonts', function (done) {
+//
+//   dutil.logMessage(task, 'Copying custom fonts');
+//
+//   var stream = gulp.src('./fonts/**/*')
+//     .pipe(gulp.dest('assets/fonts'));
+//
+//   return stream;
+//
+// // });
+//
+// gulp.task(task,
+//   gulp.series(
+//     function (done) {
+//       dutil.logMessage(task, 'Copying fonts');
+//       done();
+//     },
+//     gulp.parallel(
+//       'copy-uswds-fonts',
+//       'copy-custom-fonts'
+//     )
+//   )
+// );

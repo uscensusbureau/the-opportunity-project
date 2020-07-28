331ae275e20da00f74ca63ca3b5377a25de9b1ee
Marcella <marcellamaki@gmail.com>
2020-07-28 08:01:43 -0400

    remove console.log

--- js/toggle.js
+++ js/toggle.js
@@ -1,7 +1,6 @@
 var $ = require('jquery');
 
 $('.toggle-tip-button').on('click', function () {
-  console.log("clicking")
   if ( $(this).next('div').hasClass('modal-inactive')) {
     $(this).next('div').removeClass('modal-inactive').addClass('modal-active');
   } else {

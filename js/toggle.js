aa1f541b70e9e738af650d77eb2654d1f37b3cad
Marcella <marcellamaki@gmail.com>
2020-07-28 08:01:12 -0400

    finish toggle to learn more function

--- js/toggle.js
+++ js/toggle.js
@@ -1,6 +1,10 @@
 var $ = require('jquery');
 
 $('.toggle-tip-button').on('click', function () {
-  // console.log("clicking")
-  $( this ).next('div').removeClass('modal-inactive').addClass('modal-active');
+  console.log("clicking")
+  if ( $(this).next('div').hasClass('modal-inactive')) {
+    $(this).next('div').removeClass('modal-inactive').addClass('modal-active');
+  } else {
+    $(this).next('div').removeClass('modal-active').addClass('modal-inactive');
+  }
 });

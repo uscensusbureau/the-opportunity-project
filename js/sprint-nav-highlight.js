b6cd1ad6a96e0159f4593d19bc74d2d6354d731b
SamPotasz <sampotasznik@gmail.com>
2021-02-18 15:44:09 -0600

    improved sprint nav highlighting to not have to defer for rest of page load

--- js/sprint-nav-highlight.js
+++ js/sprint-nav-highlight.js
@@ -2,8 +2,12 @@ const nav = document.getElementById('sprint-nav')
 const items = nav.getElementsByClassName('sprint-nav-item')
 
 const highlightNav = pageName => {
-  console.log('highlighting ' + pageName)
   const toHighlight = document.getElementById( pageName ).getElementsByTagName('a')[ 0 ];
   nav.scrollTo( toHighlight.offsetLeft, toHighlight.offsetTop );
   toHighlight.classList.add('highlight')
 }
+
+const loc = window.location.pathname.replace(/\/$/, '').replace('.html', '') // get rid of trailing '/'
+const sprint = loc.substring( loc.lastIndexOf('/') + 1 )
+highlightNav( sprint );
+

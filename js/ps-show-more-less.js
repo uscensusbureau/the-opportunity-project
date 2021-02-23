5e2e5f081155b268c362e4d72013b1b834012ee6
SamPotasz <sampotasznik@gmail.com>
2021-02-23 14:14:48 -0600

    fixed scrolling position issue on problem statements expanding and collapsing

--- js/ps-show-more-less.js
+++ js/ps-show-more-less.js
@@ -23,11 +23,6 @@ if( buttons ){
           el.classList.add( COLLAPSED_CLASS );
         })
         button.innerHTML = "<span>See More</span>";
-
-        const problemHeader = parentProblem.getElementsByTagName('h3')[ 0 ]
-        console.log(window.scrollY)
-        window.scrollTo( parentProblem.offsetLeft, parentProblem.offsetTop )
-        console.log(window.scrollY)
       }
       else if( collapsedEls.length > 0 ){
         Array.from( collapsedEls ).forEach( el => {
@@ -36,6 +31,8 @@ if( buttons ){
         })
         button.innerHTML = "<span>See Less</span>";
       }
+
+      parentProblem.scrollIntoView();
     })
   })
 }

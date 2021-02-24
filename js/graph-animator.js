4610304a42053956e1cf46055a72fd0dbf04e2ea
SamPotasz <sampotasznik@gmail.com>
2021-02-24 11:21:28 -0600

    added animation to judges graphs

---
+++ js/graph-animator.js
@@ -0,0 +1,25 @@
+let options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1.0
+}
+
+let fills = document.getElementsByClassName('graph-fill');
+
+let callback = (entries, observer) => {
+  entries.forEach( entry => {
+    let intersect = entry.intersectionRatio;
+    if( intersect > 0.8 ){
+      for( fill of fills ){
+        fill.classList.remove('empty');
+      }
+    } else if( intersect <= 0.1 ){
+      for( fill of fills ){
+        fill.classList.add('empty');
+      }
+    }
+  })
+}
+
+let observer = new IntersectionObserver(callback, options);
+observer.observe( document.querySelector('#judging-graphs') );

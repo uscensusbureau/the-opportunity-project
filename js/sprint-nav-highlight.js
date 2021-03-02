f08e1f3e045aa2374bd57c2e68fb4518583dbf33
SamPotasz <sampotasznik@gmail.com>
2021-03-02 13:13:57 -0600

    Highlighting our-process subnav when navigating to new sections based on clicking the nav

--- js/sprint-nav-highlight.js
+++ js/sprint-nav-highlight.js
@@ -1,16 +1,59 @@
-const nav = document.getElementsByClassName('sprint-nav')[0];
-if( nav ){
-  const items = nav.getElementsByClassName('sprint-nav-item')
+const HIGHLIGHT_CLASS = 'highlight'
+const links = document.querySelectorAll('.sprint-nav-item a')
 
-  const highlightNav = pageName => {
-    const toHighlight = document.getElementById( pageName ).getElementsByTagName('a')[ 0 ];
-    nav.scrollTo( toHighlight.offsetLeft, toHighlight.offsetTop );
-    toHighlight.classList.add('highlight')
+
+const highlightNav = ( pageName, scrollTo ) => {
+  const section = document.getElementsByName( pageName )[ 0 ];
+  console.log('highlighting ' + pageName)
+  if( section ){
+    console.log(section)
+    const toHighlight = section.getElementsByTagName('a')[ 0 ];
+    toHighlight.classList.add(HIGHLIGHT_CLASS)
+    if( scrollTo ){
+      nav.scrollTo( toHighlight.offsetLeft, toHighlight.offsetTop )
+    }
+  }
+}
+
+const setHighlightsFromHash = () => {
+  const hash = window.location.hash.replace('#', '');
+
+  for( link of links ){
+    link.classList.remove( HIGHLIGHT_CLASS )
   }
 
-  const loc = window.location.pathname.replace(/\/$/, '').replace('.html', '') // get rid of trailing '/'
-  const sprint = loc.substring( loc.lastIndexOf('/') + 1 )
+  const li = document.getElementsByName( hash )[ 0 ]
+  if( li ){
+    const anchor = li.getElementsByTagName( 'a' )[ 0 ]
+    if( anchor ){
+      anchor.classList.add( HIGHLIGHT_CLASS )
+    }
+  } else {
+    links[ 0 ].classList.add( HIGHLIGHT_CLASS )
+  }
+}
 
-  highlightNav( sprint );
-} 
-  
+const nav = document.getElementsByClassName('sprint-nav')[0];
+if( nav ){
+  const loc = window.location.pathname.replace(/\/$/, '').replace('.html', '') // get rid of trailing '/'
+  const subLoc = loc.substring( loc.lastIndexOf('/') + 1 )
+  /*
+   * for sprints page, highlight based on /baseurl/sprints
+   * for our-process page, highlight based on /url/our-process/#id
+   */
+  if( subLoc === 'our-process' ){
+    for( link of links ){
+      // link.classList.remove( HIGHLIGHT_CLASS )
+      link.addEventListener( 'click', e => { 
+        e.preventDefault();
+        document.location = e.target.href;
+        setHighlightsFromHash();
+      })
+    }
+
+    setHighlightsFromHash();
+  }
+  else {
+    highlightNav( subLoc, true )
+  }
+}

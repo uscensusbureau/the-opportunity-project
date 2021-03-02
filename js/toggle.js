d8e2ae15465ae58b2202ca8992f051ba183fe900
SamPotasz <sampotasznik@gmail.com>
2021-03-02 11:00:27 -0600

    removed console logs

--- js/toggle.js
+++ js/toggle.js
@@ -1,8 +1,5 @@
-// var $ = require('jquery');
-
 $('.toggle-tip-button').on('click', function () {
   if ( $(this).next('div').hasClass('modal-inactive')) {
-    console.log($(this).innerHTML)
     $(this).next('div').removeClass('modal-inactive').addClass('modal-active');
     $(this).innerHTML = "- Collapse"
   } else {

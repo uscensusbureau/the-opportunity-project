98cf5543cf14877b57401ccaab35eb523c2aeb8e
SamPotasz <sampotasznik@gmail.com>
2021-03-23 13:23:02 -0500

    Moved critical.css plugin out of gulp

---
+++ config/critical-css.js
@@ -0,0 +1,42 @@
+// const gulp = require("gulp");
+const critical = require('critical') //.stream;
+
+// use this to run via node
+critical.generate({
+  base: './',
+  src: '_site/index.html',
+  css: '_site/assets/css/styles.css',
+  target: '_includes/critical.css',
+  width: 1045,
+  height: 480,
+  minify: true,
+})
+
+/**
+ * To run in gulp, use the following
+ * and un-comment line 1
+ */
+/*
+gulp.task('critical', () => {
+  return gulp
+    .src('_site/index.html')
+    .pipe(
+      critical({
+        base: './',
+        // src: '_site/index.html',
+        css: '_site/assets/css/styles.css',
+        width: 320,
+        height: 480,
+        minify: true,
+        extract: true,
+        ignore: {
+          atrule: ['@font-face'],
+        }
+      })
+    )
+    .on('error', err => {
+      log.error(err.message);
+    })
+    .pipe(gulp.dest('_includes/'));
+})
+*/

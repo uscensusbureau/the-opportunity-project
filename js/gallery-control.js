7fdb04cb953c8f3fb7e7fa3994e65b424ddfea34
Sam Potasznik <sampotasznik@gmail.com>
2021-04-16 14:05:38 -0500

    added play pause icons in css

--- js/gallery-control.js
+++ js/gallery-control.js
@@ -1,6 +1,8 @@
 const playPauseButton = document.getElementById('galleryPlayPause');
 
 if( playPauseButton ){
+  const btnDisplay = playPauseButton.children[ 0 ];
+
   playPauseButton.addEventListener( 'click', e => {
     const tiles = document.getElementsByClassName('scrolling-gallery-tile');
     for( tile of tiles ){
@@ -10,6 +12,17 @@ if( playPauseButton ){
     }
 
     const isRunningNow = tiles[0].style.animationPlayState === 'running'
-    playPauseButton.innerText = isRunningNow ? "PAUSE" : "PLAY"
+    
+    // playPauseButton.innerText = isRunningNow ? "PAUSE" : "PLAY"
+    if (isRunningNow) {
+      btnDisplay.classList.remove('play');
+      btnDisplay.classList.add('pause');
+      playPauseButton.setAttribute('aria-label', "PAUSE");
+    }
+    else {
+      btnDisplay.classList.remove('pause');
+      btnDisplay.classList.add('play');
+      playPauseButton.setAttribute('aria-label', "PLAY");
+    }
   })
 }

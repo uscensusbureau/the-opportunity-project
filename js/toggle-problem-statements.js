7ab48896f38cccbf9c68922258566cd0f190e0ed
Sam Potasznik <sampotasznik@gmail.com>
2021-05-19 14:56:43 -0500

    Converted sprint-nav-highlight, start, toggle, and toggle-problem-statements.js to standardjs

--- js/toggle-problem-statements.js
+++ js/toggle-problem-statements.js
@@ -1,26 +1,27 @@
 // Update display to reflect the selected year
 
-const tabBar = document.getElementsByClassName( 'problem-statements' )[0];
+const tabBar = document.getElementsByClassName('problem-statements')[0]
 
-if( tabBar ){
-  const yearButtons = document.getElementsByClassName( 'yr-btn' );
+if (tabBar) {
+  const yearButtons = document.getElementsByClassName('yr-btn')
 
-  tabBar.addEventListener( 'click', e => {
-    var problemSet = e.target.id
+  tabBar.addEventListener('click', e => {
+    const problemSet = e.target.id
 
-    Array.from( yearButtons ).forEach( button => button.classList.remove( 'yr-btn-active' ))
+    Array.from(yearButtons).forEach(button => button.classList.remove('yr-btn-active'))
 
-    Array.from( document.getElementsByClassName('ps-active') )
-      .forEach( el => {
-        el.classList.add( 'ps-inactive' );
-        el.classList.remove('ps-active')})
+    Array.from(document.getElementsByClassName('ps-active'))
+      .forEach(el => {
+        el.classList.add('ps-inactive')
+        el.classList.remove('ps-active')
+      })
 
-    document.getElementById(problemSet).classList.add( 'yr-btn-active' )
+    document.getElementById(problemSet).classList.add('yr-btn-active')
 
-    Array.from( document.getElementsByClassName(`ps-${problemSet}`) )
-      .forEach( el => {
-        el.classList.add( 'ps-active' )
+    Array.from(document.getElementsByClassName(`ps-${problemSet}`))
+      .forEach(el => {
+        el.classList.add('ps-active')
         el.classList.remove('ps-inactive')
       })
-  });
+  })
 }

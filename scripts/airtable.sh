dc665f64380dbf94b8073e1b9970b3d67600c7d7
Sam Potasznik <sampotasznik@gmail.com>
2021-07-23 17:02:37 -0500

    Added background image for 2020-census-data sprint page

--- scripts/airtable.sh
+++ scripts/airtable.sh
@@ -1,4 +1,4 @@
 set -o allexport
 source '.env'
-airtable-export _data $AIRTABLE_TOP_ID problem_statements agencies sprints --key=$AIRTABLE_API_KEY
+airtable-export _data $AIRTABLE_TOP_ID sprints --key=$AIRTABLE_API_KEY
 set +o allexport

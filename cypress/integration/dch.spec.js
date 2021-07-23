d7339a719ec5fb9f4942445441787b18245c5829
Sam Potasznik <sampotasznik@gmail.com>
2021-07-23 11:49:59 -0500

    Added images to all data filter headers in DCH search pages

--- cypress/integration/dch.spec.js
+++ cypress/integration/dch.spec.js
@@ -20,6 +20,12 @@ function testPSFilters (url) {
                 cy.get('h3').should('include.text', $filterId)
               })
           })
+          cy.get('[name=data-set-card].pc-inactive').each($card => {
+            cy.wrap($card)
+              .within($card => {
+                cy.get('h3').should('not.include.text', $filterId)
+              })
+          })
         }
       })
   })
@@ -33,15 +39,23 @@ describe('Content tests', () => {
         cy.wrap($card)
           .within($card => {
             cy.get('.dataset__org').invoke('text').then( text => {
-              expect(text.length).to.be.gt(0)
+              // each is 3 because of the text 'by '
+              expect(text.length).to.be.gt(3)
             })
           })
       })
     }
   })
+
+  it.only('shows datasets belonging to multiple sprints in both kits', () => {
+    cy.visit(base + '/workforce')
+    cy.get('#data-set-card-location-of-energy-infrastructure')
+    cy.visit(base + '/built-environment')
+    cy.get('#data-set-card-location-of-energy-infrastructure')
+  })
 })
 
-describe('Filtering tests', () => {
+describe.only('Filtering tests', () => {
 
   it('filters by PS on natural env page', () => {
     testPSFilters(pages[0])

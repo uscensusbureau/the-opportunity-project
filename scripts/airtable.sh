1e44841ed0bc9b3b236d97fbf433eb791e06e247
Sam Potasznik <sampotasznik@gmail.com>
2021-08-17 15:24:38 -0500

    Removed hhs news story with bad link. Replaced with Nextgov link

--- scripts/airtable.sh
+++ scripts/airtable.sh
@@ -1,4 +1,4 @@
 set -o allexport
 source '.env'
-airtable-export _data $AIRTABLE_TOP_ID sprints problem_statements products news --key=$AIRTABLE_API_KEY
+airtable-export _data $AIRTABLE_TOP_ID news --key=$AIRTABLE_API_KEY
 set +o allexport

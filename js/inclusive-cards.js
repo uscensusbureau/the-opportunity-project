04188a632b7e5fe892f0115bc27532bb9cb64a26
Sam Potasznik <sampotasznik@gmail.com>
2021-09-27 10:45:33 -0500

    Added box-shadow outlines to kit cards on hover and active

---
+++ js/inclusive-cards.js
@@ -0,0 +1,16 @@
+// const cards = document.querySelectorAll('.data-kit__card.data-kit__active')
+// for (const card of cards) {
+//   let down, up
+//   const link = card.querySelector('h2 a')
+//   card.onmousedown = () => {
+//     console.log('mousdown')
+//     down = +new Date()
+//   }
+//   card.onmouseup = () => {
+//     console.log('mouseup')
+//     up = +new Date()
+//     if ((up - down) < 200) {
+//       link.click()
+//     }
+//   }
+// }

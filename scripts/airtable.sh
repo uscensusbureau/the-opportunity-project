6e750b91aae43750427cebdc39739b62cae3071c
Logan Powell <loganp@tepper.cmu.edu>
2021-11-30 10:18:05 -0500

    runs airtable script for organizations and problem_statements to fix typo

--- scripts/airtable.sh
+++ scripts/airtable.sh
@@ -1,5 +1,11 @@
 set -o allexport
 source '.env'
 #airtable-export _data $AIRTABLE_TOP_ID people treasury_datasets --key=$AIRTABLE_API_KEY
-airtable-export _data $AIRTABLE_TOP_ID products --key=$AIRTABLE_API_KEY
+#Products:
+#airtable-export _data $AIRTABLE_TOP_ID products --key=$AIRTABLE_API_KEY
+#Organizations:
+#airtable-export _data $AIRTABLE_TOP_ID organizations --key=$AIRTABLE_API_KEY
+#Problem Statements
+airtable-export _data $AIRTABLE_TOP_ID problem_statements --key=$AIRTABLE_API_KEY
+
 set +o allexport

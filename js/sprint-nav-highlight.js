c9f0dcf935953f2e6b791f4f364780e34a742a1d
Logan Powell <loganp@tepper.cmu.edu>
2022-05-04 16:26:37 -0400

    team work!

--- js/sprint-nav-highlight.js
+++ js/sprint-nav-highlight.js
@@ -1,59 +1,64 @@
-const highlightAnchorNavigation = require('./sidenav.js')
+const highlightAnchorNavigation = require("./sidenav.js");
 
-const HIGHLIGHT_CLASS = 'highlight'
-const links = document.querySelectorAll('.sprint-nav-item a')
+const HIGHLIGHT_CLASS = "highlight";
+const links = document.querySelectorAll(".sprint-nav-item a");
 
 const highlightNav = (pageName, scrollTo) => {
-  const section = document.getElementsByName(pageName)[0]
-  let toHighlight
+  const section = document.getElementsByName(pageName)[0];
+  let toHighlight;
   if (section) {
-    toHighlight = section.getElementsByTagName('a')[0]
+    toHighlight = section.getElementsByTagName("a")[0];
   } else {
-    toHighlight = links[0]
+    toHighlight = links[0];
   }
-  toHighlight.classList.add(HIGHLIGHT_CLASS)
+  toHighlight.classList.add(HIGHLIGHT_CLASS);
   if (scrollTo) {
-    nav.scrollTo(toHighlight.offsetLeft, toHighlight.offsetTop)
+    nav.scrollTo(toHighlight.offsetLeft, toHighlight.offsetTop);
   }
-}
+};
 
 const setHighlightsFromHash = () => {
-  const hash = window.location.hash.replace('#', '')
+  const hash = window.location.hash.replace("#", "");
 
   for (const link of links) {
-    link.classList.remove(HIGHLIGHT_CLASS)
-    link.blur()
+    link.classList.remove(HIGHLIGHT_CLASS);
+    link.blur();
   }
 
-  const li = document.getElementsByName(hash)[0]
+  const li = document.getElementsByName(hash)[0];
   if (li) {
-    const anchor = li.getElementsByTagName('a')[0]
+    const anchor = li.getElementsByTagName("a")[0];
     if (anchor) {
-      anchor.classList.add(HIGHLIGHT_CLASS)
+      anchor.classList.add(HIGHLIGHT_CLASS);
     }
   } else {
-    links[0].classList.add(HIGHLIGHT_CLASS)
+    links[0].classList.add(HIGHLIGHT_CLASS);
   }
-}
+};
 
-const nav = document.getElementsByClassName('sprint-nav')[0]
+const nav = document.getElementsByClassName("sprint-nav")[0];
 if (nav) {
-  const loc = window.location.pathname.replace(/\/$/, '').replace('.html', '') // get rid of trailing '/'
-  const subLoc = loc.substring(loc.lastIndexOf('/') + 1)
+  const loc = window.location.pathname.replace(/\/$/, "").replace(".html", ""); // get rid of trailing '/'
+  console.log({ loc });
+  const subLoc = loc.substring(loc.lastIndexOf("/") + 1);
   /*
    * for our-process page, highlight based on /url/our-process/#id
    * for sprints page, highlight based on /baseurl/sprints
    */
-  if (subLoc === 'our-process') {
-    setHighlightsFromHash()
+  if (subLoc === "our-process") {
+    setHighlightsFromHash();
 
-    const headers = document.getElementsByClassName('process-section')
+    const headers = document.getElementsByClassName("process-section");
 
-    window.addEventListener('scroll', e => {
-      const navLink = highlightAnchorNavigation(links, headers, HIGHLIGHT_CLASS)
-      nav.scrollTo(navLink.offsetLeft, navLink.offsetTop)
-    })
+    window.addEventListener("scroll", (e) => {
+      const navLink = highlightAnchorNavigation(
+        links,
+        headers,
+        HIGHLIGHT_CLASS
+      );
+      nav.scrollTo(navLink.offsetLeft, navLink.offsetTop);
+    });
   } else {
-    highlightNav(subLoc, true)
+    highlightNav(subLoc, true);
   }
 }
--- scripts/airtable.sh
+++ scripts/airtable.sh
@@ -2,7 +2,8 @@ set -o allexport
 source '.env'
 #airtable-export _data $AIRTABLE_TOP_ID people treasury_datasets --key=$AIRTABLE_API_KEY
 #Products:
-airtable-export _data $AIRTABLE_TOP_ID products --key=$AIRTABLE_API_KEY
+#airtable-export _data $AIRTABLE_TOP_ID products --key=$AIRTABLE_API_KEY
+airtable-export _data $AIRTABLE_TOP_ID pages --key=$AIRTABLE_API_KEY
 #Organizations:
 #airtable-export _data $AIRTABLE_TOP_ID organizations --key=$AIRTABLE_API_KEY
 #Problem Statements
